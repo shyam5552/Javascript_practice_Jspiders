858cb56e8098253ae9060f4f65ec29bd33d57dc7
thuru <s180222@rguktsklm.ac.in>
2024-08-15 18:56:49 +0530

    string methods

---
+++ day18/stringmethods.js
@@ -0,0 +1,38 @@
+let s="java Script";
+console.log(s.length);
+console.log(s.indexOf("a"));
+console.log(s.toLowerCase());
+console.log(s.lastIndexOf("a"));
+console.log(s.charAt(0));
+console.log(s.charCodeAt(0));
+console.log(s.concat("hello"));
+console.log(s.slice(0,4));
+console.log(s.slice(-5,-1));
+console.log(s.substring(0,4));
+console.log(s.substr(0,4));
+
+console.log(s.split(""));
+console.log(s.split(" "));
+console.log(s.split());
+console.log(s.split(""));
+console.log(s.repeat(2));
+let str=" ja va ";
+console.log(str.trim());
+console.log(s.startsWith("j"));
+console.log(s.endsWith("t"));
+console.log(str.replace("ja","ba"));
+console.log(s.includes("t"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
